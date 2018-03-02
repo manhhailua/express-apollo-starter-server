d2c00e1e47861fb2cd27699e6a87316bacda2beb
Manh Pham (manhpt) <manhhailua@gmail.com>
2018-03-02 10:10:16 +0700

    add app.get('config')

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 // const favicon = require('serve-favicon');
+const nconf = require('nconf');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
+// import routes
 const index = require('./routes/index');
 
+// import middleware
 const notFoundHandler = require('./middleware/404');
 const serverErrorHandler = require('./middleware/500');
 
+// initialize app instance
 const app = express();
+
+// initialize configuration
+nconf
+  .env()
+  .argv()
+  .required(['PORT', 'MONGO_URL']);
+
+// configuration setup
+app.set('config', nconf);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,6 +38,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// attach root url
 app.use('/', index);
 
 // catch 404 and forward to error handler
